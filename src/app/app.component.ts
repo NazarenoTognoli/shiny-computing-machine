f2d2367ba6e71a710f94871c134a6423dcee2b37
NazarenoTognoli <nazarenotognoli@gmail.com>
2025-08-08 18:50:02 -0300

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { NodeComponent } from './features/node/node.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WindowComponent } from './features/window/window.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NodeComponent, CommonModule, WindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
